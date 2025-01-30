a4f1b6a1fabad1859f13cb5fb99f50928a97c22d
adamz365 <131455481+adamz365@users.noreply.github.com>
2025-01-30 08:04:02 +0100

    Add files via upload

---
+++ 28stabwounds/28stabwounds/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Transactions;
+
+namespace _28stabwounds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Message("Ahoj.");
+            int[] grades = { 1, 2, 3, 4, 5, 1, 2, 4, 3, 3, 5 };
+            int result = CalcAverage(grades);
+        }
+
+        static void Message(string message) 
+        { 
+            Console.WriteLine(message);
+        }
+
+        static int CalcAverage(int[] nums)
+        {
+            float prom;
+            foreach (int num in nums)
+            {
+
+                prom += num;
+            }
+
+            return prom / nums.Length;
+        }
+    }
+}
